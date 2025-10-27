9a89cbb5f3d9c78b0b9c160537c6128fb0a5d435
md-zeon <zeon.cse@gmail.com>
2025-10-27 13:11:00 +0600

    Hero Section Design Complete

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,23 +1,30 @@
-import { CheckCircleIcon } from 'lucide-react'
+import { CheckCircleIcon, TrendingUp } from 'lucide-react'
 import StoreButton from './button/StoreButton'
 import Image from 'next/image'
 
 const Hero = () => {
     return (
-        <section className='relative'>
+        <section className='relative overflow-hidden'>
             <div className='grid grid-cols-1 sm:grid-cols-2 px-4 pt-28 max-w-[1440px] mx-auto'>
                 {/* Left Content */}
                 <article>
                     <h3 className='text-[#787878] uppercase font-semibold mb-2'>EASY PAYMENT</h3>
-                    <div>
-                        <h1 className='text-[#01081B] text-5xl font-semibold leading-tight'>Pay fast and smarter <br /> from anywhere</h1>
-                        <Image src="/icons/underline.svg"
-                            width={350}
-                            height={12}
-                            alt="Underline"
-                            className='relative bottom-16 -right-20'
-                        />
-                    </div>
+                    <h1 className="mt-2 text-4xl text-black md:text-5xl font-bold leading-tight">
+                        <span className="relative inline-block">
+                            Pay fast and smarter
+                            <span className="absolute -bottom-1 sm:-bottom-3 right-1/4 translate-x-1/2 lg:-right-10 lg:translate-x-0 w-[200px] sm:w-[280px] lg:w-[400px]">
+                                <Image
+                                    src="/icons/underline.svg"
+                                    alt="underline vector"
+                                    width={200}
+                                    height={200}
+                                    className="w-96"
+                                />
+                            </span>
+                        </span>
+
+                        from anywhere
+                    </h1>
                     <p className='mt-2.5 mb-6'>Experience the future of payments: fast, secure, and tailored for the next generation&apos;s convenience and trust.</p>
                     {/* Buttons */}
                     <div className='flex gap-2 items-center'>
@@ -45,10 +52,38 @@ const Hero = () => {
                 </article>
                 {/* Right Content */}
                 <article>
-                    <Image src="/images/hero-image.png" alt="Hero Image" width={500} height={500} />
+                    <div className='relative'>
+                        <Image src="/images/hero-image.png" alt="Hero Image" width={500} height={500} />
+                        {/* Card 1 */}
+                        <div className='p-5 rounded-lg bg-white w-56 shadow-lg absolute bottom-10 -left-10'>
+                            <h4 className='font-semibold'>Payment Received</h4>
+                            <p className='text-[#2E68FD]'>+35,890.00</p>
+                            <div className='flex justify-between mt-2 gap-2 text-sm'>
+                                <span className='text-[#4D525F]'>1st Jan, 2025</span>
+                                <span className='text-green-500'>
+                                    3.09% <TrendingUp className='inline-block ml-1' size={16} />
+                                </span>
+                            </div>
+                        </div>
+
+                        {/* Card 2 */}
+                        <div className='flex justify-between items-center p-5 rounded-lg bg-white w-max shadow-lg absolute top-40 right-12'>
+                            <Image
+                                src="/images/avatars.png"
+                                alt='Avatars'
+                                width={96}
+                                height={40}
+                            />
+                            <div>
+                                <h4 className='font-semibold'>120K+</h4>
+                                <p className='text-sm text-[#4D525F]'>Active Users</p>
+                            </div>
+                        </div>
+                    </div>
                 </article>
             </div>
-            <Image src="/images/hero-bg.png" alt="Hero Background" layout="fill" objectFit="cover" className='-z-30 w-full h-full ' />
+            {/* Background Image */}
+            <Image src="/images/hero-bg.png" alt="Hero Background" className='-z-30 w-full h-full object-cover object-center' priority fill />
         </section>
     )
 }
--- components/button/StoreButton.tsx
+++ components/button/StoreButton.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import { Button } from "../ui/button"
+import Link from "next/link";
 
 interface StoreButtonProps {
     src: string;
@@ -9,15 +10,17 @@ interface StoreButtonProps {
 
 const StoreButton = ({ src, title, subtitle }: StoreButtonProps) => {
     return (
-        <Button className='py-2.5 px-2 w-max h-fit rounded-lg bg-foreground text-background'>
-            <div className="flex gap-2.5 items-center justify-between font-medium">
-                <Image src={src} alt={title} width={26} height={26} />
-                <div className='font-sf-compact-display'>
-                    <p className='text-xs text-start'>{subtitle}</p>
-                    <h2 className='text-xl mr-2'>{title}</h2>
+        <Link href="#">
+            <Button className='py-2.5 cursor-pointer px-2 w-max h-fit rounded-lg bg-foreground text-background'>
+                <div className="flex gap-2.5 items-center justify-between font-medium">
+                    <Image src={src} alt={title} width={26} height={26} />
+                    <div className='font-sf-compact-display'>
+                        <p className='text-xs text-start'>{subtitle}</p>
+                        <h2 className='text-xl mr-2'>{title}</h2>
+                    </div>
                 </div>
-            </div>
-        </Button>
+            </Button>
+        </Link>
     )
 }
 
